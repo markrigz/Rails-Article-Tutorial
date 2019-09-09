944565f3d75f0fd2abd36fe49fd9bb0ee3c2e5b7
markrigz <markrigz@gmail.com>
2019-09-09 18:18:42 +0100

    Added Enforced login

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@ class ArticlesController < ApplicationController
 
   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
+  #To prevent users messing with other users posts
+  before_action :require_author, only: [:edit, :update, :destroy]
+
+
   def index
     #@articles = Article.all.order(:title)
     @articles = Article.all.order(created_at: :desc)
@@ -50,6 +54,11 @@ class ArticlesController < ApplicationController
 
     redirect_to articles_path
   end
+
+  def require_author
+    redirect_to(@article) unless @article.user == current_user
+  end
+
 
   private
     def article_params
